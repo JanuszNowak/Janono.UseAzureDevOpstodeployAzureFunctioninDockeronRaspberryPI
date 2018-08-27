dbcb4575d6da1a8d3d5bff1a9972410a7c3e9f3b
Janusz Nowak <janono@gmail.com>
2018-08-27 21:27:54 +0200

    Add files via upload

---
+++ FunctionApp1/FunctionApp1/build.ps1
@@ -0,0 +1,15 @@
+﻿docker build -t azurefunctionlinux .
+docker run -p 8080:80 -it azurefunctionlinux  
+
+docker tag azurefunctionlinux janono/azurefunctionlinux
+docker push janono/azurefunctionlinux
+docker pull janono/azurefunctionlinux
+#docker version
+#docker update
+#docker ps 
+
+#docker stop $(docker ps -a -q)
+#docker rm $(docker ps -a -q)
+# Delete all images
+#docker rmi $(docker images -q)
+#
