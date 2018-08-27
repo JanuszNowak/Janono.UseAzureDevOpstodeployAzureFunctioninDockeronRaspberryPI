ff2cd057dc66e1b30f9e57c4fabe325a83e31e29
Janusz Nowak <janono@gmail.com>
2018-08-27 22:01:02 +0200

    Add files via upload

--- FunctionApp1/FunctionApp1/build.ps1
+++ FunctionApp1/FunctionApp1/build.ps1
@@ -2,14 +2,31 @@
 docker run -p 8080:80 -it azurefunctionlinux  
 
 docker tag azurefunctionlinux janono/azurefunctionlinux
+docker login
 docker push janono/azurefunctionlinux
-docker pull janono/azurefunctionlinux
+
 #docker version
 #docker update
 #docker ps 
 
-#docker stop $(docker ps -a -q)
-#docker rm $(docker ps -a -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
 # Delete all images
-#docker rmi $(docker images -q)
+docker rmi $(docker images -q)
+docker ps
+docker images
 #
+
+
+
+
+docker build -f Dockerfile.RPi3 -t azurefunctionlinuxrpi3 .
+docker run -p 8080:80 -it azurefunctionlinuxrpi3  
+
+docker tag azurefunctionlinuxrpi3 janono/azurefunctionlinuxrpi3
+docker login
+docker push janono/azurefunctionlinuxrpi3
+
+docker images
+
+#https://hub.docker.com/r/microsoft/azure-functions-runtime/tags/
