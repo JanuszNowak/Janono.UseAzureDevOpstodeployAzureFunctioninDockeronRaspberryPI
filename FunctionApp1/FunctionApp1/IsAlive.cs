fd18504b0f5641029012ed9772ed9ced6a241262
Janusz Nowak <janono@gmail.com>
2019-09-19 17:54:33 +0200

    #7 Adding Unit tests.

--- FunctionApp1/FunctionApp1/IsAlive.cs
+++ FunctionApp1/FunctionApp1/IsAlive.cs
@@ -5,18 +5,17 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
 
 namespace FunctionApp1
 {
     public static class IsAlive
     {
         [FunctionName("IsAlive")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]HttpRequest req, ILogger log)
         {
             //var a=typeof(RuntimeEnvironment).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyFileVersionAttribute>().Version;
             return (ActionResult)new OkObjectResult(new { Timestamp = DateTime.Now, Region = Environment.GetEnvironmentVariable("REGION_NAME") });
         }
-
-
     }
 }
